2061cec63f7686c02116ac662c359c11a083b48e
Dmitry <johnnysvill@gmail.com>
2026-04-30 20:34:26 +0300

    Add Load Balancer and multi-node Web Cluster

--- main.tf
+++ main.tf
@@ -36,17 +36,24 @@ resource "yandex_vpc_subnet" "my_subnet" {
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
-module "web_server" {
+module "web_servers" {
   source        = "./modules/compute"
-  
-  instance_name = "dracarys-web"
+  count         = 2
+  instance_name = "dracarys-web-${count.index}"
   subnet_id     = yandex_vpc_subnet.my_subnet.id
   ssh_pub_key   = var.public_ssh_key
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y nginx
+    echo "<h1>Hello from Web Server ${count.index}! Managed by Terraform</h1>" > /var/www/html/index.html
+    systemctl restart nginx
+  EOF
 }
 
 module "db_server" {
-  source = "./modules/compute"
-  
+  source = "./modules/compute"  
   instance_name = "dracarys-db"
   subnet_id     = yandex_vpc_subnet.my_subnet.id
   cpu_cores     = 4
@@ -54,20 +61,58 @@ module "db_server" {
   ssh_pub_key   = var.public_ssh_key
 }
 
-output "web_ip" {
-  value = module.web_server.external_ip
+resource "yandex_lb_target_group" "web_tg" {
+  name = "web-target-group"
+
+  target {
+    subnet_id = yandex_vpc_subnet.my_subnet.id
+    address   = module.web_servers[0].external_ip
+  }
+
+  target {
+    subnet_id = yandex_vpc_subnet.my_subnet.id
+    address   = module.web_servers[1].external_ip
+  }
 }
 
-output "db_ip" {
-  value = module.db_server.external_ip
-}
+resource "yandex_lb_network_load_balancer" "main_lb" {
+  name = "web-load-balancer"
 
-resource "local_file" "ansible_inventory" {
-  content = templatefile("inventory.tftpl",
-    {
-      web_ip = module.web_server.external_ip,
-      db_ip = module.db_server.external_ip
+  listener {
+    name = "http-listener"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
     }
-  )
-  filename = "hosts.ini"
+  }
+
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.web_tg.id
+    healthcheck {
+      name = "http-check"
+      http_options {
+        port = 80
+        path = "/"
+      }
+    }
+  }
 }
+
+output "load_balancer_ip" {
+  description = "ЗАЙДИ СЮДА В БРАУЗЕРЕ:"
+  value       = yandex_lb_network_load_balancer.main_lb.listener.*.external_address_spec[0].*.address
+}
+
+output "web_servers_ips" {
+  value = module.web_servers[*].external_ip
+}
+
+#resource "local_file" "ansible_inventory" {
+#  content = templatefile("inventory.tftpl",
+#    {
+#      web_ip = module.web_server.external_ip,
+#      db_ip = module.db_server.external_ip
+#    }
+#  )
+#  filename = "hosts.ini"
+#}
